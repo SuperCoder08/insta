b0650853aa27c60cc0d8d322d5bd32077f1e1b44
SuperCoder08 <152031730+SuperCoder08@users.noreply.github.com>
2024-02-07 18:51:25 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const ig = require("ig-unduh");
+
+app.get("/",(req,res)=>{
+  res.send("<h1>hello</h1>")
+});
+
+app.get("/ig",(req,res) => {
+  let link = req.query.url
+  ig(link).then(result => {
+    res.json(result)
+    console.log(result)
+  }).catch(err => {
+    console.error("Error")
+    res.send("error")
+  });
+});
+app.listen(4100,()=>{
+  console.log("Server Runing ...")
+});
